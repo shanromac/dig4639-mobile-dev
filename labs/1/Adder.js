195f3e15e35600612cf9eb204aa94a2e612e8080
Shannon McAdams <srmcadams@Shannons-MacBook-Pro.local>
2020-01-29 15:39:25 -0500

    finished Lab1

---
+++ labs/1/Adder.js
@@ -0,0 +1,15 @@
+class Adder{
+    constructor(props){
+        this.props = props;
+    }
+    sum() {
+        return (this.props.a + this.props.b);
+    }
+    render(){
+        this.sum();
+        return `<p> The sum of ${this.props.a} and ${this.props.b} is ${this.sum()}</p>`;
+    }
+}
+
+
+module.exports = Adder;
