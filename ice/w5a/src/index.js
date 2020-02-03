f771b5c3c2d1583651990d258bdec22ed567b9a1
Andrew Kaimana <an345133@ucf.edu>
2020-02-03 10:16:19 -0500

    ICE-5: TODO 2 Co-authored-by: shanromac <Srmcadams@yahoo.com>

--- ice/w5a/src/index.js
+++ ice/w5a/src/index.js
@@ -1,16 +1,35 @@
+let element = undefined;
+let taskInput;
+
 function runOnLoad()
 {
     // Create a container for us
-    let element = document.createElement("div");
+    element = document.createElement("div");
     element.id = "container";
     document.body.appendChild(element);
 
     // Handle adding a new task
     var addTaskButton = document.getElementById("addTask");
-    addTaskButton.addEventListener("click", onClick)
+    addTaskButton.addEventListener("click", onClick);
+    taskInput = document.getElementById("taskText");
 }
 function onClick() {
     console.log("clicked!");
+    let newTask = document.createElement("div");
+    let input = document.createElement("input");
+    input.type = "checkbox";
+    let span = document.createElement("span");
+    span.innerHTML = taskInput.value;
+    newTask.appendChild(input);
+    newTask.appendChild(span);
+    
+    function onChange() {
+        console.log(span.innerHTML);
+    }
+    input.addEventListener("change", onChange);
+   // newTask.innerHTML = `<input type='checkbox'></input><span>${taskInput.value}</span>`;
+
+    element.appendChild(newTask);
 }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
