7f8191048efa06bcddcb3c55be0c004f415e1e17
shanromac <srmcadams@yahoo.com>
2020-02-14 17:38:24 -0500

    Lab 3

---
+++ labs/3/src/components/Card/index.js
@@ -0,0 +1,17 @@
+import "./index.css";
+
+class App {
+    constructor(props) {
+        this.props = props;
+        this.element = document.createElement("div");
+        let span = document.createElement("span");
+        this.element.appendChild(span);
+        span.innerHTML = this.props.content;
+        this.element.className = "card";
+    }
+    render() {
+      return this.element;
+    }
+  }
+  
+  export default App;
