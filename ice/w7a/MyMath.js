337dce9769236c21c04a1cbe33c73abdb7950a16
shanromac <srmcadams@yahoo.com>
2020-02-17 10:20:36 -0500

    MyMath

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,3 +1,35 @@
+//Produces the sum of the two parameters
+/**
+ * 
+ * @param  a The first number
+ * @param  b The second number
+ */
+
 export function Sum (a, b) {
-  return a + b
+  if(typeof a == 'number' && typeof b == 'number')
+    return (a+b);
+    else
+  return undefined;
 }
+
+export function AddList(list) {
+  var result = 0;
+  for (var mem of list) {
+
+  }
+  for(var i = 0; i < list.length; i++) {
+    result = result + list[i];
+      return undefined;
+    result = result + list[i];
+  }
+  return result;
+}
+
+let op1 = 1;
+let op2 = 2;
+let result = Sum(op1,op2);
+if(result==op1+op2) {
+  console.log("It worked!");
+} else {
+  console.log("Expectd " + op1+op2 + ", but got " + result) 
+};
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,8 +1,27 @@
-import { Sum } from './sum.js'
+import { Sum } from './MyMath.js'
+
+describe('Testing AddList', () => {
+  it('Returns the sum of a list of numbers', () => 
+  {
+    let list = [1,2,3,4];
+    let result = AddList(list);
+    expect(result).toBe(10);
+  })
+})
+
+describe('Testing Sum', () => {
+  it('Adds a string and a number to get undefined', () => {
+    var result = Sum("Test", 5);
+});
 
 test('adds 1 + 2 to equal 3', () => {
   expect(Sum(1, 2)).toBe(3)
 })
+test('Border cases', () => {
+  expect(Sum(-10,5)).toBe(-5)
+  expect(Sum(1, 1)).toBe(2);
+  })
+});
 
 // ICE 
 
