820942a2a89f9068bee38a94a98fbe6fd9586211
shanromac <srmcadams@yahoo.com>
2020-02-26 09:35:50 -0500

    commit

--- demo/w8a/todo-react/src/App.js
+++ demo/w8a/todo-react/src/App.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-const todoList1 = [
-  { completed: true, priority: 1, content: 'Complete demo' },
-  { completed: true, priority: 2, content: 'Complete demo 2' },
-  { completed: true, priority: 3, content: 'Complete demo 2' },
-  { completed: true, priority: 3, content: 'Complete demo 2' },
-  { completed: true, priority: 3, content: 'Complete demo 3' }
-  ]
+import todoList from './todoList.json'
 
   function TodoItem(props) {
-    return <p>${props.content}</p>
+    return <p className='card'>{props.content}</p>
   }
 
-  function App() {
-    let array = todoList1.map((value) =>
-    <TodoItem content={value.content}
-    priority={value.priority}
-    completed={value.completed} />
-    )
-    return (
-      array
-    );
-  }
+  class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = { 
+        todoList: todoList,
+        showOnlyUncompletedTasks: false
+      }
+    }
+
+    addTask() {
+      let newtask = 
+      { "content": "Task 3", "priority": 3, "completed": false }
+      let currentList = this.state.todoList
+      currentList.push(newtask)
+      this.setState({ todoList:currentList})
+    }
+
+    render() {
+      return (
+        <>
+        <div className="inputTask">
+        <input type="button" value="Add Task" onClick={() => this.addTask()}/>
+        <br />
+        <input type="checkbox" ref="completedFilter" 
+          onChange={(e) => this.setState({ showOnlyUncompletedTasks: e.target.checked})}
+            id="completedFilter" name="completedFilter" value="completed" />
+              <label htmlFor="completedFilter"> Show only uncompleted task</label>
+        </div>
+        {(this.state.showOnlyUncompletedTasks ? this.state.todoList.filter((v) => !v.completed) : 
+          this.state.todoList).map((v, i) => <TodoItem key={i} priority={v.priority}
+            content={v.content} completed={v.completed} />)}
+          </>
+        );
+       }
+    }
 
 export default App;
