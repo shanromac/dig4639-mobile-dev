509c472cf294abcb088ac6b3d7f5d08e6aa3a673
shanromac <srmcadams@yahoo.com>
2020-04-07 22:25:47 -0400

    Lab 6

--- labs/5/src/index.js
+++ labs/5/src/index.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from "react";
+import ReactDOM, { render } from "react-dom";
+import "./assets/style.css";
+import quizBank from "./quizBank";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Lab6 extends Component {
+  state = {
+    questionBank: [],
+    score: 0,
+    responses: 0
+  };
+  getQuestions = () => {
+    quizBank().then(question => {
+      this.setState({
+        questionBank: question
+      });
+    });
+  };
+  computeAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer) {
+        this.setState({
+          score: this.state.score + 1
+        });
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+    });
+  };
+  playAgain = () => {
+    this.getQuestions();
+    this.setState({
+      score: 0,
+      responses: 0
+    });
+  };
+componentDidMount() {
+  this.getQuestions();
+}
+render() {
+  return (
+    <div className="container">
+      <div className="title">Test Your Lord of the Rings Knowledge!</div>
+      {this.state.questionBank.length > 0 &&
+      this.state.responses < 5 &&
+      this.state.questionBank.map(
+        ({question, answers, correct, questionId}) => (
+        <QuestionBox
+        question={question}
+        options={answers}
+        key={questionId}
+        selected={answer => this.computeAnswer(answer, correct)}
+        />
+        )
+      )}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      {this.state.responses === 5 ? (
+      <Result score={this.state.score} playAgain={this.playAgain} />) : null}
+    </div>
+    );
+  }
+}
+
+ReactDOM.render(<Lab6 />, document.getElementById("root"));
